2a14870249841a4d4c2df196c37a23a6438caf76
ka1sser <serenokaisser@gmail.com>
2023-09-20 23:05:37 +0800

    Basic gameplay wrap up

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -7,11 +7,13 @@ public class CrashDetector : MonoBehaviour
     [SerializeField] float sceneTimeDelay = 0.25f;
     [SerializeField] ParticleSystem crashEffect;
     [SerializeField] AudioClip crashSFX;
+    bool hasCrashed = false;
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.CompareTag("Ground"))
+        if(other.CompareTag("Ground") && !hasCrashed)
         {
+            hasCrashed = true;
             FindObjectOfType<PlayerController>().DisableControls();
             crashEffect.Play();
             GetComponent<AudioSource>().PlayOneShot(crashSFX);
